16871375933dadf6fb0e695d3b966d4219dd6724
DangKimAn <dangkiman41005@gmail.com>
2026-03-30 23:02:58 +0700

    fix : add ProductCard and fix return of function Hompage in file page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,21 @@
+import ProductCard from "../components/ProductCard";
+import Navbar from "../components/Navbar"
+import Button from "../components/Button"
 async function getProducts() {
     const res = await fetch('https://fakestoreapi.com/products');
     return res.json();
 }
 export default async function HomePage() {
     const products = await getProducts();
+
+    return (
+        <>
+        <Navbar/>
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
         {products.map(item => (
             <ProductCard key={item.id} product={item} />
         ))}
     </div>
+    </>
+    )
 }
